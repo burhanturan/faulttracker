dc0512227b7c726e4c5bd363d336b53563fc9bf9
Turan <burhanturan.elk@gmail.com>
2025-12-03 20:34:22 +0300

    is_dev:false

--- lib/api.ts
+++ lib/api.ts
@@ -4,7 +4,7 @@ import { Platform } from 'react-native';
 // Use localhost for web/iOS simulator, 10.0.2.2 for Android emulator, and local IP for real device
 // Use localhost for web/iOS simulator, 10.0.2.2 for Android emulator, and local IP for real device
 // CHANGE THIS TO 'true' WHEN DEVELOPING LOCALLY
-const IS_DEV = true;
+const IS_DEV = false;
 
 export const BASE_URL = IS_DEV
     ? Platform.select({
